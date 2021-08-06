a6028103bf2dd85b0b77d16acbb31e2b77ea7580
vsard123 <vsard123@gmail.com>
2021-08-06 18:35:01 +0700

    commit in new brand

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,14 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function FirstPost() {
-    return (
-        <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </>
-    )
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home Page</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
